867e8df6c8d0f7b8d3ad4dfd488198591d32f75c
arielszy <79292265+arielszy@users.noreply.github.com>
2021-04-13 13:08:57 -0300

    Update Big.dart

--- lib/Big.dart
+++ lib/Big.dart
@@ -1,1 +1,20 @@
-H
+Agregar notifylesteners a la clase with changenotifier
+
+Agregar el if al consumer para ver cuando se completa el build
+
+
+
+Map<String, String> listMapToMap(List<Generos> listaGeneros){
+
+
+Map<String, String> mapaGeneros={};
+for (var i =0; i>=; i++){
+mapaGeneros.addAll(ListaGeneros[i]['id']:ListaGeneros[i]['name']);
+
+
+}
+Print(mapaGeneros);
+Return mapaGeneros;
+
+}
+
